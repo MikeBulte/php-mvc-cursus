db1f631fe10c83254b05951bfab4ac7d230adda5
Mike <mikebulte@roc-dev.com>
2017-05-16 11:11:36 +0200

    nutteloze variabelen verwijderd

--- opdrachten/02GETIntroductie.php
+++ opdrachten/02GETIntroductie.php
@@ -8,11 +8,4 @@
 <body>
 </body>
 </html>
-<?php
-    $naam = "Mike Bulte";
-    $adres = "1820 ZW, Urk";
-    $email = "'professional'coder67@stackoverflow.com";
-    $telefoon = "044-2918124";
-    $land = "Nederland";
-
-    echo "Mijn naam is ".$_GET['naam']." Ik woon in ".$_GET['land']." op ".$_GET['adres']." Mijn email is ".$_GET['email']." en mijn telefoon nummer is ".$_GET['telefoon'];
+<?= "Mijn naam is ".$_GET['naam']." Ik woon in ".$_GET['land']." op ".$_GET['adres']." Mijn email is ".$_GET['email']." en mijn telefoon nummer is ".$_GET['telefoon']; ?>
