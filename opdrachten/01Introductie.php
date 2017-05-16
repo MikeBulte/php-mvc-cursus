97ff312473051974e76e805c4ed10e01fbb98f62
Mike <mikebulte@roc-dev.com>
2017-05-16 10:18:00 +0200

    Initial Commit

---
+++ opdrachten/01Introductie.php
@@ -0,0 +1,8 @@
+<?php
+    $naam = "Mike Bulte";
+    $adres = "1820 ZW, Urk";
+    $email = "'professional'coder67@stackoverflow.com";
+    $telefoon = "044-2918124";
+    $land = "Nederland";
+
+    echo "Mijn naam is $naam. Ik woon in $land op $adres. Mijn email is $email en mijn telefoon nummer is $telefoon.";
